c008703f0eeb97fbe7549ed2f0445672a0c5a789
Youri Tomassen <youritomassen@gmail.com>
2017-05-03 16:50:05 +0200

    Initial commit

---
+++ app/src/main/java/ap/tomassen/online/ruigetweets/MainActivity.java
@@ -0,0 +1,68 @@
+package ap.tomassen.online.ruigetweets;
+
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String filename = "output.json";
+
+        try {
+            String JsonString = readAssetIntoString(filename);
+            JSONObject jsonObject = new JSONObject(JsonString);
+            System.out.println(jsonObject.toString());
+        } catch (JSONException je) {
+            je.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Reads an asset file and returns a string with the full contents.
+     *
+     * @param filename The filename of the file to read.
+     * @return The contents of the file.
+     * @throws IOException If file could not be found or not read.
+     */
+    private String readAssetIntoString(String filename) throws IOException {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            InputStream is = getAssets().open(filename, AssetManager.ACCESS_BUFFER);
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
