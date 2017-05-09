7d62c851cfe930b2e5201b4c1f080b21a7bd9e81
Youri Tomassen <youritomassen@gmail.com>
2017-05-09 10:45:16 +0200

    Merge master origin/master

---
+++ app/src/main/java/ap/tomassen/online/ruigetweets/entities/Hashtag.java
@@ -0,0 +1,23 @@
+package ap.tomassen.online.ruigetweets.entities;
+
+import ap.tomassen.online.ruigetweets.Entity;
+
+/**
+ * Created by Eric on 8-5-2017.
+ */
+
+public class Hashtag extends Entity {
+    private String text;
+
+    public Hashtag(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
