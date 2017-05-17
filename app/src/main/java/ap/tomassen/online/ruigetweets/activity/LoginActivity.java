c49cbec4cefb43b481b90156747d2967b0e81793
Youri Tomassen <youritomassen@gmail.com>
2017-05-17 13:48:02 +0200

    Made progress on LoginActivity

--- app/src/main/java/ap/tomassen/online/ruigetweets/activity/LoginActivity.java
+++ app/src/main/java/ap/tomassen/online/ruigetweets/activity/LoginActivity.java
@@ -1,10 +1,18 @@
 package ap.tomassen.online.ruigetweets.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+import com.github.scribejava.core.oauth.OAuth10aService;
+
+import java.io.IOException;
+
 import ap.tomassen.online.ruigetweets.R;
+import ap.tomassen.online.ruigetweets.model.TwitterApi;
 
 /**
  * Created by youri on 17-5-2017.
@@ -15,5 +23,31 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        System.out.println(new RequestTokens());
+    }
+
+    private class RequestTokens extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            final OAuth10aService authService = new ServiceBuilder()
+                    .apiKey("sUWAnBCiS9ssPIlNDWeby1ol4")
+                    .apiSecret("YMpi7BcHd57TWMNIfS9A1lymuulnm0lXKjlpeqgsGmoj2nUZrZ")
+                    .callback("http://www.erickerkhoven.nl")
+                    .build(TwitterApi.getInstance());
+
+            String url = null;
+            try {
+                OAuth1RequestToken reqToken = authService.getRequestToken();
+                url = authService.getAuthorizationUrl(reqToken);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return url;
+
+        }
+
     }
 }
--- app/src/main/java/ap/tomassen/online/ruigetweets/model/TwitterApi.java
+++ app/src/main/java/ap/tomassen/online/ruigetweets/model/TwitterApi.java
@@ -1,15 +1,25 @@
 package ap.tomassen.online.ruigetweets.model;
 
+import com.github.scribejava.core.builder.api.DefaultApi10a;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+
 /**
  * Created by youri on 17-5-2017.
  */
 
-public class TwitterApi {
-    private String token;
+public class TwitterApi extends DefaultApi10a {
 
     private static TwitterApi api = null;
 
+    private String requestToken;
+    private String authorizeToken;
+    private String accessToken;
+
+
     private TwitterApi() {
+        requestToken = "https://api.twitter.com/oauth2/token";
+        authorizeToken = "https://api.twitter.com/oauth/request_token";
+        accessToken = "https://api.twitter.com/oauth/access_token";
     }
 
     public static TwitterApi getInstance() {
@@ -17,5 +27,20 @@ public class TwitterApi {
             api = new TwitterApi();
         }
         return api;
+    }
+
+    @Override
+    public String getRequestTokenEndpoint() {
+        return null;
+    }
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return null;
+    }
+
+    @Override
+    public String getAuthorizationUrl(OAuth1RequestToken requestToken) {
+        return null;
     }
 }
