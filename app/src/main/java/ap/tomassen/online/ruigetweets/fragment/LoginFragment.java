bbe9da2960a02b8c0aece90b232d4ef2e52aa92f
Youri Tomassen <youritomassen@gmail.com>
2017-05-18 13:21:58 +0200

    Changes onclickListner in LoginFragment.xml to Inner clicklistener

--- app/src/main/java/ap/tomassen/online/ruigetweets/fragment/LoginFragment.java
+++ app/src/main/java/ap/tomassen/online/ruigetweets/fragment/LoginFragment.java
@@ -3,6 +3,7 @@ package ap.tomassen.online.ruigetweets.fragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
@@ -16,26 +17,38 @@ import ap.tomassen.online.ruigetweets.R;
  * Created by Eric on 17-5-2017.
  */
 
-public class LoginFragment extends Fragment implements View.OnClickListener{
+public class LoginFragment extends Fragment {
+
+    private CallBackListener context;
+
     public LoginFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.login_fragment, container, false);
 
-        Button btn_login = (Button) view.findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(this);
+        Button btnLogin = (Button) view.findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TwitterFragment twitterFragment = new TwitterFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fl_fragment_container, twitterFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
 
         return view;
     }
 
-    @Override
-    public void onClick(View v) {
-        TwitterFragment twitterFragment =new TwitterFragment();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fl_fragment_container, twitterFragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+    public interface CallBackListener {
+        public void LoginCallback();
     }
 }
